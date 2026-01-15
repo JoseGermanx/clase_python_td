7b6e436e96881ef8980138ebd80e5f4dbee660d8
Jose G Martinez <jgermanmm@gmail.com>
2026-01-14 22:19:07 -0300

    Add room reservation functionality with error handling and total cost calculation

--- ejercicio_reserva_hotel.py
+++ ejercicio_reserva_hotel.py
@@ -13,6 +13,71 @@
 # 5️⃣ Mostrar el resultado en pantalla:
 # 6️⃣ Manejo de errores:
 
+
+# habitaciones
+# 1 simple,
+# 2 doble,
+# 3 suite 
+
+habitaciones = {
+    1: {"categoria": "simple", "precio": 50},
+    2: {"categoria": "doble", "precio": 80},
+    3: {"categoria": "suite", "precio": 150},
+    4: {"categoria": "presidencial", "precio": 250}
+}
+
+
+#Menú de bienvenida
+print("Bienvenido al hotel Paraiso")
+print("Opciones disponibles")
+for num, datos in habitaciones.items():
+    # 1. Habitación simple - Precio: 50/noche
+    print(f"{num}. Habitación {datos["categoria"]} - Precio: {datos["precio"]}/noche.")
+try:
+    seleccion = int(input("Ingresa que tipo de habitación deseas: [1-4]: "))
+
+    habitación_seleccionada = habitaciones.get(seleccion)
+
+    #validar si exite el tipo de habitación y la cantidad de noches es mayor a 0
+    if habitación_seleccionada:
+        cantidad_de_noches = int(input("Ingresa la cantidad de noches: "))
+        if cantidad_de_noches > 0:
+            total_a_pagar = habitación_seleccionada["precio"] * cantidad_de_noches
+
+            # Generar reserva
+            print("---Datos de su reserva-----")
+            print(f"Habitación {habitación_seleccionada["categoria"]}")
+            print(f"Precio unitario: {habitación_seleccionada["precio"]}")
+            print(f"Total de la reserva: {total_a_pagar}")
+        else:
+            print("Error: La cantidad de noches debe ser mayor a 0")
+    else:
+         print("Tipo de habitación no exite.")
+except ValueError:
+    print("Error: Por favor ingresa un número entero!!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Preguntas de Reflexión:
 
 # ¿Cómo mejoraría este sistema con más funcionalidades (por ejemplo, descuentos por estadías largas)?
