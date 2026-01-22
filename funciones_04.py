bff80bacc301a2bf3cf5a8fbe4b0b8fda7080fff
Jose G Martinez <jgermanmm@gmail.com>
2026-01-21 21:28:50 -0300

    funciones_04

---
+++ funciones_04.py
@@ -0,0 +1,18 @@
+
+
+def saludar(nombre):
+    print(f"Hola, {nombre}. Bienvenido!!")
+
+
+saludar("Ana")
+saludar("Luis")
+
+
+def sumar(a, b):
+    return a + b
+
+resultado = sumar(3,5)
+resultado_2 = sumar(10,56)
+
+print(resultado)
+print(resultado_2)
